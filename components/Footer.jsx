f6889cf1dc0dd16c00e69fd676720487947f5b18
shouvikbj <bajpayeeshouvik@gmail.com>
2022-10-26 20:35:47 +0530

    fixed navigation bar for all sized screens

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,30 +1,38 @@
 import React from 'react';
-import { MDBFooter, MDBContainer, MDBBtn, MDBIcon } from 'mdb-react-ui-kit'
+import { MDBFooter, MDBContainer, MDBBtn, MDBIcon, MDBRow, MDBCol } from 'mdb-react-ui-kit'
 import Link from 'next/link';
 
 const Footer = () => {
   return (
     <MDBFooter className='text-center fixed-bottom'>
-      <div className='text-center p-3 m-2' style={{
-        "backgroundColor": "#27292b",
-        "borderRadius": "90px"
-      }}>
-        <Link href="/">
-          <MDBBtn floating size='lg' tag='a' outline style={{"marginRight":"40px", "border": "none"}}>
-            <MDBIcon fas size='xl' icon="list" color='white' />
-          </MDBBtn>
-        </Link>
-        <Link href="/add">
-          <MDBBtn floating size='lg' tag='a' outline style={{"marginRight":"40px", "marginLeft": "40px", "border": "none"}}>
-            <MDBIcon fas size='xl' icon='add' color='white' />
-          </MDBBtn>
-        </Link>
-        <Link href="/categories">
-          <MDBBtn floating size='lg' tag='a' outline style={{"marginLeft":"40px", "border": "none"}}>
-            <MDBIcon fas size='xl' icon='folder' color='white' />
-          </MDBBtn>
-        </Link>
-      </div>
+      <MDBContainer>
+        <MDBRow className='mb-3'>
+          <MDBCol sm='4'></MDBCol>
+          <MDBCol sm='4'>
+            <div className='text-center p-3 m-2' style={{
+              "backgroundColor": "#27292b",
+              "borderRadius": "90px"
+            }}>
+              <Link href="/">
+                <MDBBtn floating size='lg' tag='a' outline style={{"marginRight":"40px", "border": "none"}}>
+                  <MDBIcon fas size='xl' icon="list" color='white' />
+                </MDBBtn>
+              </Link>
+              <Link href="/add">
+                <MDBBtn floating size='lg' tag='a' outline style={{"marginRight":"40px", "marginLeft": "40px", "border": "none"}}>
+                  <MDBIcon fas size='xl' icon='add' color='white' />
+                </MDBBtn>
+              </Link>
+              <Link href="/categories">
+                <MDBBtn floating size='lg' tag='a' outline style={{"marginLeft":"40px", "border": "none"}}>
+                  <MDBIcon fas size='xl' icon='folder' color='white' />
+                </MDBBtn>
+              </Link>
+            </div>
+          </MDBCol>
+          <MDBCol sm='4'></MDBCol>
+        </MDBRow>
+      </MDBContainer>
     </MDBFooter>
   )
 }
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,27 +2,19 @@ import React from 'react'
 import Head from 'next/head'
 import Navbar from './Navbar'
 import Footer from './Footer'
-import {MDBContainer, MDBRow, MDBCol} from "mdb-react-ui-kit"
+// import {MDBContainer, MDBRow, MDBCol} from "mdb-react-ui-kit"
 
 const Layout = ({children}) => {
   return (
     <div style={{"backgroundColor": "#0D0D0D"}}>
-        <Head>
-          <title>Tasks Web</title>
-          <link rel="manifest" href="/manifest.json" />
-          <script src="/app.js" async />
-        </Head>
-        {/*<Navbar/> */}
-        {children}
-        <MDBContainer>
-          <MDBRow className='mb-3'>
-            <MDBCol sm='4'></MDBCol>
-            <MDBCol sm='4'>
-              <Footer/>
-            </MDBCol>
-            <MDBCol sm='4'></MDBCol>
-          </MDBRow>
-        </MDBContainer>
+      <Head>
+        <title>Tasks Web</title>
+        <link rel="manifest" href="/manifest.json" />
+        <script src="/app.js" async />
+      </Head>
+      {/*<Navbar/> */}
+      {children}
+      <Footer/>
     </div>
   )
 }
